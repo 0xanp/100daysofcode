72f58f483c1ff5fb52f6dc83230f45a43143c6f7
Binh An Pham <anbinhpham110799@gmail.com>
2022-08-08 18:57:16 -0400

    intitialized multi-page structure

---
+++ Intro.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("100 days of code (Currently Day 1)")
+    st.write("Week 1 to Week 4: Basics, Data Structures and Algorithms")
+    st.write("Week 5 to Week 12: In-depth Practice with Leetcode")
+
+if __name__ == "__main__":
+    main()
---
+++ pages/1_Basics.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.title("The Basics")
+    # Day 1
+    with st.expander("Day 1"):
+        st.markdown("### Array")
+    with st.expander("Day 2"):
+        st.markdown("### String")
+
+if __name__ == "__main__":
+    main()
